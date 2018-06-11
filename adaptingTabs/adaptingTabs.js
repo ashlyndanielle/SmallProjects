5cff24286d2d7611d74de2b673a52d8f89ad7833
Ashlyn Mitros <ashlyndmitros@gmail.com>
2018-06-11 13:45:37 -0600

    added credit line and ability to click out of menu to close

--- adaptingTabs/adaptingTabs.js
+++ adaptingTabs/adaptingTabs.js
@@ -28,56 +28,70 @@ window.addEventListener('resize', doAdapt);
 
 // open and close "more" menu on click
 moreContainer.addEventListener('click', e => {
-  e.preventDefault();
-  // onclick, toggle the secondary list
-  container.classList.toggle('show-secondary');
-  // set aria-expanded based on existance of 'show-secondary'
-  moreButton.setAttribute('aria-expanded', container.classList.contains('show-secondary'));
+	e.preventDefault();
+	// onclick, toggle the secondary list
+	container.classList.toggle('show-secondary');
+	// set aria-expanded based on existance of 'show-secondary'
+	moreButton.setAttribute('aria-expanded', container.classList.contains('show-secondary'));
+})
+
+
+// allow user to click outside of the dropdown to close
+document.addEventListener('click', e => {
+	let element = e.target;
+	while(element) {
+		if ( element === secondary || element === moreContainer ) {
+			return;
+		}
+		element = element.parentNode;
+	}
+	container.classList.remove('show-secondary');
+	moreContainer.setAttribute('aria-expanded', false);
 })
 
 
 
-// function to manage the "more" menu
+// manage the "more" menu
 function doAdapt() {
-  // make all menu items visible
-  allListItems.forEach(item => {
-    item.classList.remove('hidden');
-  });
+	// make all menu items visible
+	allListItems.forEach(item => {
+		item.classList.remove('hidden');
+	});
 
-  let stopWidth = moreContainer.offsetWidth;
-  const primaryWidth = primary.offsetWidth;
+	let stopWidth = moreContainer.offsetWidth;
+	const primaryWidth = primary.offsetWidth;
 
-  let hiddenItems = [];
+	let hiddenItems = [];
 
-  // hide items from nav when necessary
-  primaryItems.forEach((item, i) => {
-    // if the width of the primary nav is greater or equal to
-    // the width of the "more" button + the width of the item...
-    if (primaryWidth >= stopWidth + item.offsetWidth) {
-      // ...then add the item's width to the stopWidth
-      stopWidth += item.offsetWidth;
-    } else {
-      // hide the item
-      item.classList.add('hidden');
-      // add the hidden item to the hiddenItems list
-      hiddenItems.push(i);
-    }
-  })
+	// hide items from nav when necessary
+	primaryItems.forEach((item, i) => {
+		// if the width of the primary nav is greater or equal to
+		// the width of the "more" button + the width of the item...
+		if (primaryWidth >= stopWidth + item.offsetWidth) {
+			// ...then add the item's width to the stopWidth
+			stopWidth += item.offsetWidth;
+		} else {
+			// hide the item
+			item.classList.add('hidden');
+			// add the hidden item to the hiddenItems list
+			hiddenItems.push(i);
+		}
+	})
 
-  // add only hidden items to secondary menu
-  if (!hiddenItems.length) {
-    // hide the "more" button
-    moreContainer.classList.add('hidden');
-    // remove unnecessary 'show-secondary' class
-    container.classList.remove('show-secondary');
-    moreButton.setAttribute('aria-expanded', false);
-  } else {
-    // if there are items in the hidden list...
-    secondaryItems.forEach((item, i) => {
-      // if the hidden list doesn't include the item, hide it
-      if (!hiddenItems.includes(i)) {
-        item.classList.add('hidden');
-      }
-    })
-  }
+	// add only hidden items to secondary menu
+	if (!hiddenItems.length) {
+		// hide the "more" button
+		moreContainer.classList.add('hidden');
+		// remove unnecessary 'show-secondary' class
+		container.classList.remove('show-secondary');
+		moreButton.setAttribute('aria-expanded', false);
+	} else {
+		// if there are items in the hidden list...
+		secondaryItems.forEach((item, i) => {
+			// if the hidden list doesn't include the item, hide it
+			if (!hiddenItems.includes(i)) {
+				item.classList.add('hidden');
+			}
+		})
+	}
 }
