1f69d0f1bd4a4cb0b4e354cabcf1c5d194da724f
Ashlyn Mitros <ashlyndmitros@gmail.com>
2018-06-11 13:21:40 -0600

    more button only displays if there are hidden items and only displays the hidden items

--- adaptingTabs/adaptingTabs.js
+++ adaptingTabs/adaptingTabs.js
@@ -5,7 +5,7 @@ const primaryItems = document.querySelectorAll('.primary > li:not(.more)');
 // remove css fix once js has loaded
 container.classList.add('jsfied');
 
-// inswert "more" button and duplicate the list
+// insert "more" button and duplicate the list
 primary.insertAdjacentHTML('beforeend', `
   <li class="more">
     <button type="button" aria-haspopup="true" aria-expanded="false">More &darr;</button>
@@ -15,18 +15,69 @@ primary.insertAdjacentHTML('beforeend', `
   </li>
 `);
 
-
 const secondary = document.querySelector('.secondary');
 const secondaryItems = secondary.querySelectorAll('li');
-const allListItmes = document.querySelectorAll('li');
-const moreLi = document.querySelector('.more');
-const moreButton = moreLi.querySelector('button');
+const allListItems = document.querySelectorAll('li');
+const moreContainer = document.querySelector('.more');
+const moreButton = moreContainer.querySelector('button');
 
 
-moreLi.addEventListener('click', e => {
+doAdapt();
+window.addEventListener('resize', doAdapt);
+
+
+// open and close "more" menu on click
+moreContainer.addEventListener('click', e => {
   e.preventDefault();
   // onclick, toggle the secondary list
   container.classList.toggle('show-secondary');
   // set aria-expanded based on existance of 'show-secondary'
   moreButton.setAttribute('aria-expanded', container.classList.contains('show-secondary'));
 })
+
+
+
+// function to manage the "more" menu
+function doAdapt() {
+  // make all menu items visible
+  allListItems.forEach(item => {
+    item.classList.remove('hidden');
+  });
+
+  let stopWidth = moreContainer.offsetWidth;
+  const primaryWidth = primary.offsetWidth;
+
+  let hiddenItems = [];
+
+  // hide items from nav when necessary
+  primaryItems.forEach((item, i) => {
+    // if the width of the primary nav is greater or equal to
+    // the width of the "more" button + the width of the item...
+    if (primaryWidth >= stopWidth + item.offsetWidth) {
+      // ...then add the item's width to the stopWidth
+      stopWidth += item.offsetWidth;
+    } else {
+      // hide the item
+      item.classList.add('hidden');
+      // add the hidden item to the hiddenItems list
+      hiddenItems.push(i);
+    }
+  })
+
+  // add only hidden items to secondary menu
+  if (!hiddenItems.length) {
+    // hide the "more" button
+    moreContainer.classList.add('hidden');
+    // remove unnecessary 'show-secondary' class
+    container.classList.remove('show-secondary');
+    moreButton.setAttribute('aria-expanded', false);
+  } else {
+    // if there are items in the hidden list...
+    secondaryItems.forEach((item, i) => {
+      // if the hidden list doesn't include the item, hide it
+      if (!hiddenItems.includes(i)) {
+        item.classList.add('hidden');
+      }
+    })
+  }
+}
